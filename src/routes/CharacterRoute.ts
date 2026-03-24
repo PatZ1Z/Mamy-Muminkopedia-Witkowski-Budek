32e65c460a381e3e052c102f91a627c8c8f5f706
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2026-03-24 14:21:37 +0100

    make requests in requests.http

--- src/routes/CharacterRoute.ts
+++ src/routes/CharacterRoute.ts
@@ -1,13 +1,14 @@
 import express from "express";
+import {getTasks, postTask, putTask, deleteTask} from "../controllers/character.controller";
 
 const router = express.Router();
-//do zrobienia
-/*router.get('/', getTasks);
+
+router.get('/', getTasks);
 
 router.post('/', postTask);
 
 router.put('/:id', putTask);
 
-router.delete('/:id', deleteTask);*/
+router.delete('/:id', deleteTask);
 
 export default router;
